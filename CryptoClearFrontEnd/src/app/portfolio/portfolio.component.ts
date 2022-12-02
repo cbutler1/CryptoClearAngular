9def4827abbd8c573cd232e8e22aa4c5216b3e5a
Brennan <brennan.davis@outlook.com>
2022-12-01 20:40:55 -0500

    Fixed Table Data Display

--- CryptoClearFrontEnd/src/app/portfolio/portfolio.component.ts
+++ CryptoClearFrontEnd/src/app/portfolio/portfolio.component.ts
@@ -2,26 +2,45 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { CryptoServiceService } from '../crypto-service.service';
 import { Transaction } from '../interfaces';
-import { Coin } from '../interfaces-coins';
+import { Coin, CoinSimple } from '../interfaces-coins';
 
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
-  styleUrls: ['./portfolio.component.css']
+  styleUrls: ['./portfolio.component.css'],
 })
 export class PortfolioComponent implements OnInit {
-currentPortfolio: Transaction[] = [];
+  currentPortfolio: Transaction[] = [];
 
-  constructor(private _service: CryptoServiceService) { }
+  constructor(private _service: CryptoServiceService) {}
 
   ngOnInit(): void {
-    this.loadCurrentPortfolio(1)
-  };
+    this.loadCurrentPortfolio(1);
+  }
 
   loadCurrentPortfolio = (userId: number): void => {
     this._service.getTransactions(userId).subscribe((data: Transaction[]) => {
       this.currentPortfolio = data;
       console.log(data);
-  });
-}
+    });
+  };
+
+  loadSiblingPrice = (e: HTMLElement) => {
+    let sibling = e.previousElementSibling;
+    let currentPrice = sibling?.innerHTML;
+    console.log(currentPrice);
+
+    if (currentPrice) {
+      let cPrice = parseInt(currentPrice);
+      console.log(cPrice);
+
+      e.innerText = `${cPrice}`;
+    }
+  };
+
+  getCurrentPrice = (coinId: string): any => {
+    this._service.getSingleCoinPrice(coinId).subscribe((data: CoinSimple) => {
+      return data.coin.price;
+    });
+  };
 }
